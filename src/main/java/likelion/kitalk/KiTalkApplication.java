11cc1ff0aa788d55149d95f9f0c5ab20685e06fb
chaeelin <chaeee0123@gmail.com>
2025-08-21 18:04:21 +0900

    :tada: Start: 프로젝트 생성

---
+++ src/main/java/likelion/kitalk/KiTalkApplication.java
@@ -0,0 +1,13 @@
+package likelion.kitalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiTalkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KiTalkApplication.class, args);
+  }
+
+}
---
+++ src/test/java/likelion/kitalk/KiTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.kitalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiTalkApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
