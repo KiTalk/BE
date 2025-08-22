45c1fe3c0e761d121bba12d290fe27ca6b39d08a
chaeelin <chaeee0123@gmail.com>
2025-08-23 03:56:30 +0900

    :sparkles: Feat: 메뉴 에러코드 추가

--- src/main/java/likelion/kitalk/touch/exception/MenuErrorCode.java
+++ src/main/java/likelion/kitalk/touch/exception/MenuErrorCode.java
@@ -12,6 +12,9 @@ public enum MenuErrorCode implements BaseErrorCode {
   INVALID_CATEGORY("M001", "유효하지 않은 카테고리입니다.", HttpStatus.BAD_REQUEST),
   EMPTY_CATEGORY_RESULT("M002", "해당 카테고리에 조회된 메뉴가 없습니다.", HttpStatus.NOT_FOUND),
 
+  // 메뉴 관련 에러
+  MENU_NOT_FOUND("M006", "메뉴를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+
   // 서버 에러
   MENU_LIST_FETCH_ERROR("M003", "메뉴 목록 조회 중 서버 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
   CATEGORY_LIST_FETCH_ERROR("M004", "카테고리 목록 조회 중 서버 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
